5b36fa31200e6dbdd2a72b0c49a82ad0d52abf92
Mark Bekooy <markb@delta-n.nl>
2025-07-31 22:30:46 +0200

    feat: Update lefthook configuration to use pnpm

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0, "never", ["sentence-case", "start-case", "pascal-case"]],
+  },
   ignores: [message => message.startsWith("chore: bump")], // Ignore dependabot commits
 };
 
