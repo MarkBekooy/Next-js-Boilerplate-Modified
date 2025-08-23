3c2983a7ee04ef0b44ce445a1e860123893f27dc
Mark Bekooy <markb@delta-n.nl>
2025-08-23 16:18:08 +0200

    feat: Update database schema example with timestamps and users schema

--- src/libs/db/schema.ts
+++ src/libs/db/schema.ts
@@ -1,26 +1,33 @@
-import { integer, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 import { v7 as uuidv7 } from "uuid";
 
 // This file defines the structure of your database tables using the Drizzle ORM.
 
 // To modify the database schema:
 // 1. Update this file with your desired changes.
-// 2. Generate a new migration by running: `npm run db:generate`
+// 2. Generate a new migration by running: `pnpm db:generate --name=<migration-name>`
 
 // The generated migration file will reflect your schema changes.
 // The migration is automatically applied during the next database interaction,
 // so there's no need to run it manually or restart the Next.js server.
 
-// Need a database for production? Check out https://www.prisma.io/?via=nextjsboilerplate
-// Tested and compatible with Next.js Boilerplate
+const timestamps = {
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow().$onUpdate(() => new Date()).notNull(),
+};
 
-// FIXME: Remove and replace this example schema with own table schema
-export const counterSchema = pgTable("counter", {
+export const usersSchema = pgTable("users", {
   id: uuid("id").primaryKey().$defaultFn(uuidv7),
-  count: integer("count").default(0),
-  updatedAt: timestamp("updated_at", { mode: "date" })
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
-  createdAt: timestamp("created_at", { mode: "date" }).defaultNow().notNull(),
-});
+  email: text("email").notNull().unique(),
+  firstName: text("first_name"),
+  lastName: text("last_name"),
+  authUserId: text("auth_user_id").notNull().unique(),
+  stripeCustomerId: text("stripe_customer_id").unique(),
+  isAdmin: boolean("is_admin").default(false).notNull(),
+  ...timestamps,
+  deletedAt: timestamp("deleted_at", { withTimezone: true }),
+}, table => [
+  uniqueIndex("email_idx").on(table.email),
+  uniqueIndex("user_auth_user_id_idx").on(table.authUserId),
+  uniqueIndex("user_stripe_customer_id_idx").on(table.stripeCustomerId),
+]);
