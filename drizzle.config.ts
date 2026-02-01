e7bb3df41711c8093d49aba2aa417d15c09e6229
Mark Bekooy <markb@delta-n.nl>
2026-02-01 17:10:34 +0100

    fix: Don't run environment variable check with Drizzle when generating migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "drizzle-kit";
-import { Env } from "./src/libs/Env";
 
 export default defineConfig({
   out: "./migrations",
   schema: "./src/libs/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: Env.DATABASE_URL ?? "",
+    url: process.env.DATABASE_URL ?? "",
   },
   verbose: true,
   strict: true,
